e2ae188666beb1f60fe40df52fcfaae190b004f2
frostland7 <cebanvitalii45@gmail.com>
2025-12-11 22:57:49 +0200

    Update firebase.js
    Signed-off-by: frostland7 <cebanvitalii45@gmail.com>

--- s/js/firebase.js
+++ s/js/firebase.js
@@ -1,17 +1,22 @@
-// Подключение Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Вставь сюда свой firebaseConfig
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  databaseURL: "...", // URL базы
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyAFsqArlLDAFYqTEOmw3vbmfIrzjQFEyp8",
+  authDomain: "xgram-80081.firebaseapp.com",
+  databaseURL: "https://xgram-80081-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "xgram-80081",
+  storageBucket: "xgram-80081.firebasestorage.app",
+  messagingSenderId: "875987331702",
+  appId: "1:875987331702:web:365cd672ea72b8712a0d57",
+  measurementId: "G-75NZFVHPKY"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+const analytics = getAnalytics(app);
